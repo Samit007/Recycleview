d5170bbe222f509acf8474a3671712921f1ec108
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 12:00:20 +0545

    Recycle View displayed in linear layout

--- app/src/main/java/Adapter/ContactsAdapter.java
+++ app/src/main/java/Adapter/ContactsAdapter.java
@@ -1,23 +1,59 @@
 package Adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.recycleview.R;
 
+import java.util.List;
+
+import Model.Contacts;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ContactsAdapter {
-    public  class ContactsViewHolder extends RecyclerView.ViewHolder{
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
+
+    Context mContext;
+    List<Contacts> contactsList;
+
+    public ContactsAdapter(Context mContext, List<Contacts> contactsList) {
+        this.mContext = mContext;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsAdapter.ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact, viewGroup, false);
+        return new ContactsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsAdapter.ContactsViewHolder contactsViewHolder, int i) {
+        Contacts contacts = contactsList.get(i);
+        contactsViewHolder.imgProfile.setImageResource(contacts.getImageId());
+        contactsViewHolder.tvName.setText(contacts.getName());
+        contactsViewHolder.tvPhone.setText(contacts.getPhoneNo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
+    public class ContactsViewHolder extends RecyclerView.ViewHolder {
         CircleImageView imgProfile;
         TextView tvName, tvPhone;
-        public ContactsViewHolder(@NonNull View itemView){
+
+        public ContactsViewHolder(@NonNull View itemView) {
             super(itemView);
-            imgProfile= itemView.findViewById(R.id.imgProfile);
-            tvName= itemView.findViewById(R.id.tvName);
-            tvPhone= itemView.findViewById(R.id.tvPhone);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvPhone = itemView.findViewById(R.id.tvPhone);
         }
     }
 }
--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.recycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.ContactsAdapter;
+import Model.Contacts;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerview);
+
+        //create list of contacts
+        List<Contacts> contactsList = new ArrayList<>();
+        contactsList.add(new Contacts("Saugat Malla","9808025412",R.drawable.saugat));
+        contactsList.add(new Contacts("Rajesh Hamal","9808025413",R.drawable.rajesh));
+        contactsList.add(new Contacts("Dahayang Rai","9808025414",R.drawable.dahayang));
+        contactsList.add(new Contacts("Bhuwan KC","90808025415",R.drawable.bhuwan));
+
+        ContactsAdapter contactsAdapter=new ContactsAdapter(this,contactsList);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
